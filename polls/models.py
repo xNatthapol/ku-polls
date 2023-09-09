ab403bb434de6ba61092db3f71f3edb7b3c1e9e7
Natthapol SERMSARAN <natthapol.se@ku.th>
2023-09-10 00:50:52 +0700

    add a default for the pub_date

--- polls/models.py
+++ polls/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField("date published")
+    pub_date = models.DateTimeField("date published", auto_now_add=True)
     end_date = models.DateTimeField("date ended", null=True, blank=True)
 
     def was_published_recently(self):
