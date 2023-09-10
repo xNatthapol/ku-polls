02b90a29b23cf025efca4c53c903150ab96a108c
Natthapol SERMSARAN <natthapol.se@ku.th>
2023-09-10 20:05:47 +0700

    add 4 unit tests for can_vote

--- polls/tests.py
+++ polls/tests.py
@@ -60,6 +60,37 @@ class QuestionModelTests(TestCase):
         past_pub_date = Question(pub_date=time)
         self.assertIs(past_pub_date.is_published(), True)
 
+    def test_cannot_vote_after_end_date(self):
+        """
+        can_vote() should return False if the question has an end_date in the past.
+        """
+        time = timezone.now() - datetime.timedelta(days=30)
+        end_date_in_past = Question(end_date=time)
+        self.assertIs(end_date_in_past.can_vote(), False)
+
+    def test_can_vote_before_end_date(self):
+        """
+        can_vote() should return True if the question has an end_date in the future.
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        end_date_in_future = Question(end_date=time)
+        self.assertIs(end_date_in_future.can_vote(), True)
+
+    def test_can_vote_if_no_end_date(self):
+        """
+        can_vote() should return True if the question has no end_date (end_date is null).
+        """
+        no_end_date = Question(end_date=None)
+        self.assertIs(no_end_date.can_vote(), True)
+
+    def test_can_vote_if_end_date_is_now(self):
+        """
+        can_vote() should return True if the question has an end_date that is now.
+        """
+        time = timezone.now()
+        end_date_is_now = Question(end_date=time)
+        self.assertIs(end_date_is_now.can_vote(), True)
+
 def create_question(question_text, days):
     """
     Create a question with the given `question_text` and published the
