6f95b89bbdeec3daed005615e3fff1da037cee4b
Natthapol SERMSARAN <natthapol.se@ku.th>
2023-09-17 20:11:39 +0700

    remove the attribute votes and setter in Choice model

--- polls/models.py
+++ polls/models.py
@@ -42,17 +42,11 @@ class Choice(models.Model):
     """
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
 
     @property
     def votes(self):
         """Return the number count"""
         return self.vote_set.count()
-
-    @votes.setter
-    def votes(self, value):
-        """Set the number of votes"""
-        self._votes = value
 
     def __str__(self):
         return self.choice_text
