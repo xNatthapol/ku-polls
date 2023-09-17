f3fd8dc4fcbd7a508366d6070008040251430285
Natthapol SERMSARAN <natthapol.se@ku.th>
2023-09-17 20:13:29 +0700

    add test for adding a choice to a question

---
+++ polls/tests/test_choice.py
@@ -0,0 +1,19 @@
+"""Tests of Choice."""
+import datetime
+from django.utils import timezone
+from django.test import TestCase
+from polls.models import Question, Choice
+
+
+class ChoiceTest(TestCase):
+    def test_add_choice(self):
+        """Test adding a choice to a question."""
+        time = timezone.now() + datetime.timedelta(days=-5)
+        # Create a question
+        question = Question.objects.create(question_text="Question test 001", pub_date=time)
+        # Create a choice
+        choice = Choice.objects.create(question=question, choice_text="choice test 001")
+
+        # Check that the choice was created
+        self.assertEqual(question.choice_set.count(), 1)
+        self.assertEqual(choice.choice_text, "choice test 001")
